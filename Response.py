1f30ab2476bea42e6cccc32d4d9f71be31799e61
sam byford <sambyford11@hotmail.co.uk>
2019-10-26 17:26:30 +0100

    altered app.run

--- Response.py
+++ Response.py
@@ -25,4 +25,4 @@ def incoming_sms():
     return str(resp)
 
 if __name__ == "__main__":
-    app.run(debug=True, port=os.getenv('PORT', 5000))
+    app.run(debug=True, port=os.getenv('PORT', 5000), host='0.0.0.0')
