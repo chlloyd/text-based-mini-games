4b635639e1f885f4b02c783c4e91ff563a14e769
unknown <chris.lloyd2504@gmail.com>
2019-10-26 15:10:53 +0100

    Adding returning of random word

--- hangman/hangman.py
+++ hangman/hangman.py
@@ -1,3 +1,11 @@
 import random
 
 
+class HangmanGame:
+    def __init__(self):
+        with open('words.txt') as f:
+            self.lines = f.read().splitlines()
+
+    def random_word(self):
+        return self.lines[random.randint(0, len(self.lines))].lower()
+
