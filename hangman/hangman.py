69f1fc6d463faa5e4c862ed5c19f232fc7e89fd9
unknown <chris.lloyd2504@gmail.com>
2019-10-26 23:19:03 +0100

    adding more hangman functionality

--- hangman/hangman.py
+++ hangman/hangman.py
@@ -2,30 +2,27 @@ import random
 
 from lives import LIVES
 
+with open('words.txt') as f:
+    lines = f.read().splitlines()
 
-class HangmanGame:
-    def __init__(self):
-        with open('words.txt') as f:
-            self.lines = f.read().splitlines()
+def random_word():
+    return lines[random.randint(0, len(lines))].lower()
 
-    def random_word(self):
-        return self.lines[random.randint(0, len(self.lines))].lower()
+def first_message(word):
+    return "Welcome to hangman. Your word is " + str(
+        len(word)) + " letters long. \nYou currently have 10 lives left. \nEnter your first letter! \n" + LIVES[0]
 
-    def first_message(self, word):
-        return "Welcome to hangman. Your word is " + str(len(word)) + " letters long"
+def run_game(input, word):
+    hearts = 10
+    while hearts > 0:
+        if len(input) != 1:
+            print("Please enter 1 letter")
+        else:
+            if input in word:
+                # create ---letter--- line
+                pass
+            else:
+                hearts -= 1
 
-
-    def run_game(self):
-        word = self.random_word()
-        print(self.first_message(word))
-        """running = True
-        while running == True:
-            if letter in word:"""
-
-
-
-
-
-playGame = HangmanGame()
-playGame.run_game()
+run_game("aa", random_word())
 
