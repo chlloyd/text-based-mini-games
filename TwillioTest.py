b493453322efc5c3eff36d8eb31eb690ca30560a
sam byford <sambyford11@hotmail.co.uk>
2019-10-26 13:28:53 +0100

    Code to call TwillioAPI

---
+++ TwillioTest.py
@@ -0,0 +1,15 @@
+from twilio.rest import Client
+
+# Your Account SID from twilio.com/console
+account_sid = "ACcd8a78d406587b726168bc01c495bec4"
+# Your Auth Token from twilio.com/console
+auth_token  = "9c6463bb4da05690907deda767681baa"
+
+client = Client(account_sid, auth_token)
+
+message = client.messages.create(
+    to="+447950878755",
+    from_="+447480536962",
+    body="Hello from Python again!")
+
+print(message.sid)
