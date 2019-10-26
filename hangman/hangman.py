f390ef40c7cae321c1981d529ecd5503c8e08757
unknown <chris.lloyd2504@gmail.com>
2019-10-26 18:21:03 +0100

    Added starting message

--- hangman/hangman.py
+++ hangman/hangman.py
@@ -1,5 +1,7 @@
 import random
 
+from lives import LIVES
+
 
 class HangmanGame:
     def __init__(self):
@@ -8,4 +10,22 @@ class HangmanGame:
 
     def random_word(self):
         return self.lines[random.randint(0, len(self.lines))].lower()
+
+    def first_message(self, word):
+        return "Welcome to hangman. Your word is " + str(len(word)) + " letters long"
+
+
+    def run_game(self):
+        word = self.random_word()
+        print(self.first_message(word))
+        """running = True
+        while running == True:
+            if letter in word:"""
+
+
+
+
+
+playGame = HangmanGame()
+playGame.run_game()
 
