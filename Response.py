bd3d730f92d8939c4b82e29e9f7af980b48526fd
sam byford <sambyford11@hotmail.co.uk>
2019-10-26 17:49:41 +0100

    Began Menu

--- Response.py
+++ Response.py
@@ -1,26 +1,30 @@
 from flask import Flask, request, redirect
 from twilio.twiml.messaging_response import MessagingResponse
 import os
+
 app = Flask(__name__)
+start = False
 
 @app.route("/", methods=['GET'])
 def test():
     return 'hello'
 
 @app.route("/sms", methods=['GET','POST'])
-def incoming_sms():
+def Start():
     """Send a dynamic reply to an incoming text message"""
     # Get the message the user sent our Twilio number
     body = request.values.get('Body', None)
-
     # Start our TwiML response
     resp = MessagingResponse()
 
-    # Determine the right reply for this message
-    if body == 'hello':
-        resp.message("Hi!")
-    elif body == 'bye':
-        resp.message("Goodbye")
+    while start == False:
+        if body.lower() == 'start game':
+            resp.message("Hello and welcome to Text Based Mini Games by Murray's Angels \n hhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh")
+            start = True
+        else:
+            resp.message("Please enter one of the following commands; \n '- Start Game'")
+    #else:
+        # call games depending on options
 
     return str(resp)
 
